73d33343bb4274f0fde2bd7dcae334bd192cf9e8
Robert Glaser <robertbglaser@gmail.com>
2018-11-25 17:56:28 -0800

    the looping is correct now. 1,2 3 2 1

--- script.js
+++ script.js
@@ -2,38 +2,24 @@
         arrowLeft = document.querySelector("#arrow-left"),
         arrowRight = document.querySelector("#arrow-right"),
         bannerStatus = 1,
-        bannerTimer = 5000;
+        bannerTimer = 4000;
+        let prevBanner = 0;
 
         console.log("in script.js")
 
         window.onload = function(){
             console.log("onload")
-            bannerLoopSwitch(1);
+            bannerLoop();
         }
 
-       var startBannerLoop = setInterval(function(){
-            bannerLoopSwitch(2)
-            bannerLoopSwitch(3);
-            bannerLoopSwitch(2);
-            bannerLoopSwitch(1);
-            clearInterval(startBannerLoop);
-        }, bannerTimer); 
+        var startBannerLoop = setInterval(function(){
+            bannerLoop()
+        }, bannerTimer);
+        clearInterval(startBannerLoop);
 
-        var imgOrder = [2, 3, 2, 1];
-
-      /*  setInterval(function(){
+        function bannerLoop(){
             
-            for (img in imgOrder){
-                console.log("startBannerLoop " + img)
-                bannerLoopSwitch(img);
-            }
-            
-        }, bannerTimer);  */
-
-     /*   function bannerLoop(){
-            console.log("in bannerLoop, the value of status is " + bannerStatus)
             if (bannerStatus === 1){ 
-                console.log("bannerStatus = 1 ")
                 document.getElementById("imgban2").style.opacity = "0";
                 setTimeout(function(){
                     document.getElementById("imgban1").style.right = "0px";
@@ -42,13 +28,13 @@
                     document.getElementById("imgban2").style.zIndex = "1500px";
                     document.getElementById("imgban3").style.right = "1200px";
                     document.getElementById("imgban3").style.zIndex = "500px"
-                }, 500);
+                }, 1000);
                 setTimeout(function(){
                     document.getElementById("imgban2").style.opacity = "1";
-                }, 1000);
+                }, 500);
                     bannerStatus = 2;
-            } else if (bannerStatus === 2) {
-                console.log("bannerStatus = 2 ")
+                    prevBanner = 1;
+            } else if (bannerStatus == 2) {
                 document.getElementById("imgban3").style.opacity = "0";
                 setTimeout(function(){
                     document.getElementById("imgban2").style.right = "0px";
@@ -57,14 +43,19 @@
                     document.getElementById("imgban3").style.zIndex = "1500px";
                     document.getElementById("imgban1").style.right = "1200px";
                     document.getElementById("imgban1").style.zIndex = "500px"
-                }, 500);
+                }, 750);
                 setTimeout(function(){
                     document.getElementById("imgban3").style.opacity = "1";
-                }, 1000);
-                bannerStatus = 3;
+                }, 500);
+                if (prevBanner == 3){
+                    prevBanner = 2
+                    bannerStatus = 1;
+                }else{ 
+                    bannerStatus = 3;
+                }
+                
             }
-            else if (bannerStatus === 3) {
-                console.log("in the final else ")
+            else if (bannerStatus == 3) {
                 document.getElementById("imgban1").style.opacity = "0";
                 setTimeout(function(){
                     document.getElementById("imgban3").style.right = "0px";
@@ -73,88 +64,17 @@
                     document.getElementById("imgban1").style.zIndex = "1500px";
                     document.getElementById("imgban2").style.right = "1200px";
                     document.getElementById("imgban2").style.zIndex = "500px"
-                }, 500);
+                }, 750);
                 setTimeout(function(){
                     document.getElementById("imgban1").style.opacity = "1";
                 }, 1000)
                 
-                bannerStatus = 1;
+                bannerStatus = 2;
+                prevBanner = 3;
             }
             
-        } */
-
-        function bannerLoopSwitch(imgNo){
-            let imgNumber = 0;
-            if (typeof imgNo === "string"){
-                console.log ("string found " + imgNo );
-                imgNumber =  parseInt(imgNo)
-            }
-            else {
-                imgNumber =imgNo
-            }
-                
-
-            console.log("inside of the switch" + imgNo);
-
-            console.log (typeof imgNo);
-            switch (imgNo){
-
-                case 1:{
-                    console.log("switch 1")
-                    document.getElementById("imgban2").style.opacity = "0";
-                    setTimeout(function(){
-                        document.getElementById("imgban1").style.right = "0px";
-                        document.getElementById("imgban1").style.zIndex = "1000px";
-                        document.getElementById("imgban2").style.right = "-1200px";
-                        document.getElementById("imgban2").style.zIndex = "1500px";
-                        document.getElementById("imgban3").style.right = "1200px";
-                        document.getElementById("imgban3").style.zIndex = "500px"
-                    }, 500);
-                    setTimeout(function(){
-                        document.getElementById("imgban2").style.opacity = "1";
-                    }, 1000);
-                        //bannerStatus = 2;
-                    break;
-                }
-                case 2:{
-                    console.log("switch 2")
-                document.getElementById("imgban3").style.opacity = "0";
-                setTimeout(function(){
-                    document.getElementById("imgban2").style.right = "0px";
-                    document.getElementById("imgban2").style.zIndex = "1000px";
-                    document.getElementById("imgban3").style.right = "-1200px";
-                    document.getElementById("imgban3").style.zIndex = "1500px";
-                    document.getElementById("imgban1").style.right = "1200px";
-                    document.getElementById("imgban1").style.zIndex = "500px"
-                }, 500);
-                setTimeout(function(){
-                    document.getElementById("imgban3").style.opacity = "1";
-                }, 1000);
-                //bannerStatus = 3;
-                    break;
-                }
-                case 3:{
-                    console.log("switch 3")
-                document.getElementById("imgban1").style.opacity = "0";
-                setTimeout(function(){
-                    document.getElementById("imgban3").style.right = "0px";
-                    document.getElementById("imgban3").style.zIndex = "1000px";
-                    document.getElementById("imgban1").style.right = "-1200px";
-                    document.getElementById("imgban1").style.zIndex = "1500px";
-                    document.getElementById("imgban2").style.right = "1200px";
-                    document.getElementById("imgban2").style.zIndex = "500px"
-                }, 500);
-                setTimeout(function(){
-                    document.getElementById("imgban1").style.opacity = "1";
-                }, 1000)
-               // bannerStatus = 1;
-                break;
-                }
-            }
         }
 
 
 
-
-
-      
+     
