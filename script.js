ae1568e4cc8faf4b6771748da30f190163f1b405
Robert Glaser <robertbglaser@gmail.com>
2018-11-25 16:16:01 -0800

    checkpoint.  most functionality working, need to get the order correct.

--- script.js
+++ script.js
@@ -1,47 +1,160 @@
-
     let sliderImages = document.querySelectorAll('.slide'),
         arrowLeft = document.querySelector("#arrow-left"),
         arrowRight = document.querySelector("#arrow-right"),
-        current = 0;
+        bannerStatus = 1,
+        bannerTimer = 5000;
 
-        function reset(){
-            for (let i = 0; i<sliderImages.length; i++){
-                sliderImages[i].style.display = 'none';
+        console.log("in script.js")
+
+        window.onload = function(){
+            console.log("onload")
+            bannerLoopSwitch(1);
+        }
+
+       var startBannerLoop = setInterval(function(){
+            bannerLoopSwitch(2)
+            bannerLoopSwitch(3);
+            bannerLoopSwitch(2);
+            bannerLoopSwitch(1);
+            clearInterval(startBannerLoop);
+        }, bannerTimer); 
+
+        var imgOrder = [2, 3, 2, 1];
+
+      /*  setInterval(function(){
+            
+            for (img in imgOrder){
+                console.log("startBannerLoop " + img)
+                bannerLoopSwitch(img);
+            }
+            
+        }, bannerTimer);  */
+
+     /*   function bannerLoop(){
+            console.log("in bannerLoop, the value of status is " + bannerStatus)
+            if (bannerStatus === 1){ 
+                console.log("bannerStatus = 1 ")
+                document.getElementById("imgban2").style.opacity = "0";
+                setTimeout(function(){
+                    document.getElementById("imgban1").style.right = "0px";
+                    document.getElementById("imgban1").style.zIndex = "1000px";
+                    document.getElementById("imgban2").style.right = "-1200px";
+                    document.getElementById("imgban2").style.zIndex = "1500px";
+                    document.getElementById("imgban3").style.right = "1200px";
+                    document.getElementById("imgban3").style.zIndex = "500px"
+                }, 500);
+                setTimeout(function(){
+                    document.getElementById("imgban2").style.opacity = "1";
+                }, 1000);
+                    bannerStatus = 2;
+            } else if (bannerStatus === 2) {
+                console.log("bannerStatus = 2 ")
+                document.getElementById("imgban3").style.opacity = "0";
+                setTimeout(function(){
+                    document.getElementById("imgban2").style.right = "0px";
+                    document.getElementById("imgban2").style.zIndex = "1000px";
+                    document.getElementById("imgban3").style.right = "-1200px";
+                    document.getElementById("imgban3").style.zIndex = "1500px";
+                    document.getElementById("imgban1").style.right = "1200px";
+                    document.getElementById("imgban1").style.zIndex = "500px"
+                }, 500);
+                setTimeout(function(){
+                    document.getElementById("imgban3").style.opacity = "1";
+                }, 1000);
+                bannerStatus = 3;
+            }
+            else if (bannerStatus === 3) {
+                console.log("in the final else ")
+                document.getElementById("imgban1").style.opacity = "0";
+                setTimeout(function(){
+                    document.getElementById("imgban3").style.right = "0px";
+                    document.getElementById("imgban3").style.zIndex = "1000px";
+                    document.getElementById("imgban1").style.right = "-1200px";
+                    document.getElementById("imgban1").style.zIndex = "1500px";
+                    document.getElementById("imgban2").style.right = "1200px";
+                    document.getElementById("imgban2").style.zIndex = "500px"
+                }, 500);
+                setTimeout(function(){
+                    document.getElementById("imgban1").style.opacity = "1";
+                }, 1000)
+                
+                bannerStatus = 1;
+            }
+            
+        } */
+
+        function bannerLoopSwitch(imgNo){
+            let imgNumber = 0;
+            if (typeof imgNo === "string"){
+                console.log ("string found " + imgNo );
+                imgNumber =  parseInt(imgNo)
+            }
+            else {
+                imgNumber =imgNo
+            }
+                
+
+            console.log("inside of the switch" + imgNo);
+
+            console.log (typeof imgNo);
+            switch (imgNo){
+
+                case 1:{
+                    console.log("switch 1")
+                    document.getElementById("imgban2").style.opacity = "0";
+                    setTimeout(function(){
+                        document.getElementById("imgban1").style.right = "0px";
+                        document.getElementById("imgban1").style.zIndex = "1000px";
+                        document.getElementById("imgban2").style.right = "-1200px";
+                        document.getElementById("imgban2").style.zIndex = "1500px";
+                        document.getElementById("imgban3").style.right = "1200px";
+                        document.getElementById("imgban3").style.zIndex = "500px"
+                    }, 500);
+                    setTimeout(function(){
+                        document.getElementById("imgban2").style.opacity = "1";
+                    }, 1000);
+                        //bannerStatus = 2;
+                    break;
+                }
+                case 2:{
+                    console.log("switch 2")
+                document.getElementById("imgban3").style.opacity = "0";
+                setTimeout(function(){
+                    document.getElementById("imgban2").style.right = "0px";
+                    document.getElementById("imgban2").style.zIndex = "1000px";
+                    document.getElementById("imgban3").style.right = "-1200px";
+                    document.getElementById("imgban3").style.zIndex = "1500px";
+                    document.getElementById("imgban1").style.right = "1200px";
+                    document.getElementById("imgban1").style.zIndex = "500px"
+                }, 500);
+                setTimeout(function(){
+                    document.getElementById("imgban3").style.opacity = "1";
+                }, 1000);
+                //bannerStatus = 3;
+                    break;
+                }
+                case 3:{
+                    console.log("switch 3")
+                document.getElementById("imgban1").style.opacity = "0";
+                setTimeout(function(){
+                    document.getElementById("imgban3").style.right = "0px";
+                    document.getElementById("imgban3").style.zIndex = "1000px";
+                    document.getElementById("imgban1").style.right = "-1200px";
+                    document.getElementById("imgban1").style.zIndex = "1500px";
+                    document.getElementById("imgban2").style.right = "1200px";
+                    document.getElementById("imgban2").style.zIndex = "500px"
+                }, 500);
+                setTimeout(function(){
+                    document.getElementById("imgban1").style.opacity = "1";
+                }, 1000)
+               // bannerStatus = 1;
+                break;
+                }
             }
         }
 
-        
 
-        function startSlide(){
-            reset();
-            sliderImages[0].style.display = "block";
-        }
 
-        function slideLeft (){
-            reset();
-            sliderImages[current - 1].style.display = "block";
-            current--;
-        }
 
-        function slideRight(){ 
-            reset();
-            sliderImages[current +1].style.display = "block";
-            current++;
 
-         }
-
-    /*    arrowLeft.addEventListener('click', function(){
-            if (current === 0){
-                current = sliderImages.length;
-                slideLeft();
-            }
-       }); */
-
-       /* arrowRight.addEventListener('click', function(){
-            if (current === sliderImages.length - 1){
-                current = -1
-            }
-            slideRight();
-        }); */
-        
-        startSlide();
+      
